0ccc9ca0f9e3f3d20f4799fce7545c2ff1d1c9ce
akramoo <samirounis2@gmail.com>
2024-11-18 16:52:29 +0100

    Server fucntion to fetch model respond from the backend

---
+++ frontend/src/lib/Action/model.ts
@@ -0,0 +1,30 @@
+"use server";
+
+export const getModelRespond = async (formData: FormData) => {
+  const superficie = formData.get("superficie");
+
+  if (!superficie) {
+    throw new Error("Land details are required.");
+  }
+
+  try {
+    const response = await fetch("http://127.0.0.1:8000/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ superficie }), // Send data as JSON
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "An unknown error occurred.");
+    }
+
+    const data = await response.json(); // Parse JSON response
+    return data;
+  } catch (error: any) {
+    console.error("Failed to fetch prediction:", error);
+    throw new Error(error.message || "Network error occurred.");
+  }
+};
