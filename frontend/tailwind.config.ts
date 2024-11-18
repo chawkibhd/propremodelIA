e160b1b2347484993527d4214315c71b9d91f518
akramoo <samirounis2@gmail.com>
2024-11-18 16:49:54 +0100

    init frontend

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      backgroundImage: {
+        home: "url('/images/home.jpg')"
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
