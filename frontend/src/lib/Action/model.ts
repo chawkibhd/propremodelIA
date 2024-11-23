bf8dce18f55552a5d401046ab7fcb686f738f242
akramoo <samirounis2@gmail.com>
2024-11-23 10:47:28 +0100

    change the endpoint

--- frontend/src/lib/Action/model.ts
+++ frontend/src/lib/Action/model.ts
@@ -8,7 +8,7 @@ export const getModelRespond = async (formData: FormData) => {
   }
 
   try {
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch("/api/predict", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
