d541942fde3c910f310045a8306a43cf26d0c2e6
akramoo <samirounis2@gmail.com>
2024-11-23 10:11:51 +0100

    remove metadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,11 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import HomeLyout from "@/components/layout/Home";
 
-export const metadata = {
-  title: "Predict Land Price",
-  description: "An application to predict land prices based on input details.",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
