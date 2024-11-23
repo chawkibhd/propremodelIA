239f115ef6a801b4038a5a6ac67f36d9003f8135
akramoo <samirounis2@gmail.com>
2024-11-23 15:09:17 +0100

    change the origin and version

--- frontend/api/server.py
+++ frontend/api/server.py
@@ -7,13 +7,7 @@ import numpy as np
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://10.0.2.2:8000",
-    "http://192.168.1.100:8000",
-]
+origins = ["*"]
 
 
 app.add_middleware(
